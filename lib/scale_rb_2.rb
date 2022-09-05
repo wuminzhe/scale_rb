34ddd5278efa5cd6c5acb67a130bb7aa27cfb4c8
Aki Wu <aki.wu@itering.com>
2022-09-05 12:44:53 +0800

    add logger

--- lib/scale_rb_2.rb
+++ lib/scale_rb_2.rb
@@ -1,53 +1,8 @@
+# frozen_string_literal: true
+
 require 'scale_rb_2/version'
-
-class String
-  def to_bytes
-    data = start_with?('0x') ? self[2..] : self
-    raise 'Not valid hex string' if data =~ /[^\da-f]+/i
-
-    data = "0#{data}" if data.length.odd?
-    data.scan(/../).map(&:hex)
-  end
-end
-
-class Integer
-  def to_bytes
-    to_s(16).to_bytes
-  end
-end
-
-class Array
-  def to_hex
-    raise 'Not a byte array' unless byte_array?
-
-    reduce('0x') { |hex, byte| hex + byte.to_s(16).rjust(2, '0') }
-  end
-
-  def to_bin
-    raise 'Not a byte array' unless byte_array?
-
-    reduce('0b') { |bin, byte| bin + byte.to_s(2).rjust(8, '0') }
-  end
-
-  def to_utf8
-    raise 'Not a byte array' unless byte_array?
-
-    pack('C*').force_encoding('utf-8')
-  end
-
-  def to_scale_uint
-    reverse.to_hex.to_i(16)
-  end
-
-  def flip
-    reverse
-  end
-
-  def byte_array?
-    all? { |e| e >= 0 and e <= 255 }
-  end
-end
-require 'scale_rb_2/version'
+require 'monkey_patching'
+require 'logger'
 
 def array?(type)
   type[0] == '[' && type[type.length - 1] == ']'
@@ -79,6 +34,7 @@ def parse_fixed_array(type)
   [inner_type, length]
 end
 
+# main module
 module ScaleRb2
   class Error < StandardError; end
   # decoding errors
@@ -90,15 +46,24 @@ module ScaleRb2
   # encoding errors
   class LengthNotEqualErr < Error; end
 
-  def self.decode(type, bytes)
-    puts '============================================================================================================='
-    puts "           type: #{type}"
-    puts "          bytes: #{bytes}"
-    value, remaining_bytes = do_decode(type, bytes)
-    puts "        decoded: #{value}"
-    puts "remaining bytes: #{remaining_bytes}"
-    [value, remaining_bytes]
+  class << self
+    attr_writer :logger
+
+    def logger
+      @logger ||= Logger.new($stdout)
+    end
+
+    def decode(type, bytes)
+      logger.debug 'DECODING ========================================================================================='
+      logger.debug "           type: #{type}"
+      logger.debug "          bytes: #{bytes}"
+      value, remaining_bytes = do_decode(type, bytes)
+      logger.debug "        decoded: #{value}"
+      logger.debug "remaining bytes: #{remaining_bytes}"
+      [value, remaining_bytes]
+    end
   end
+
 
   def self.do_decode(type, bytes)
     return decode_compact(bytes) if type == 'Compact'
