acdd0effac7fc03c07685e7ff1f94a7adf337b92
Aki Wu <aki.wu@itering.com>
2022-09-10 16:24:47 +0800

    add complex array decoding test

--- spec/registry_spec.rb
+++ spec/registry_spec.rb
@@ -30,4 +30,24 @@ RSpec.describe ScaleRb2 do
     type = ScaleRb2.get_final_type_from_registry(registry, 'CustomType')
     expect(type).to eql('Vec<u8>')
   end
+
+  it 'decode complex array' do
+    registry = {
+      'CustomType1' => 'Type1',
+      'Type1' => 'u8',
+      'CustomType2' => 'u16'
+    }
+    type = '[([CustomType1; 3], CustomType2); 2]'
+    bytes = [
+      0x12, 0x34, 0x56, 0x2e, 0xfb,
+      0x12, 0x34, 0x56, 0x2e, 0xfb,
+      0x78
+    ]
+    arr, remaining_bytes = ScaleRb2.do_decode(type, bytes, registry)
+    expect(arr).to eql([
+                         [[0x12, 0x34, 0x56], 64_302],
+                         [[0x12, 0x34, 0x56], 64_302]
+                       ])
+    expect(remaining_bytes).to eql([0x78])
+  end
 end
