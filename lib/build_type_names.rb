0cd87c12376bdc2e1da7c970aaeab0c23b19dbd1
Aki Wu <aki.wu@itering.com>
2022-09-18 09:04:12 +0800

    update

--- lib/build_type_names.rb
+++ lib/build_type_names.rb
@@ -12,13 +12,13 @@
 #     name => id
 #   }
 # }
-def build_type_names(types, id_name_map = {})
+def build_type_names(types, id_name_map = {}, name_id_map = {})
   types.each_key do |id|
-    _get_name(id, id_name_map, types)
+    _get_name(id, id_name_map, name_id_map, types)
   end
 end
 
-def _get_name(id, id_name_map, types)
+def _get_name(id, id_name_map, name_id_map, types)
   return id_name_map[id] unless id_name_map[id].nil?
 
   path = types[id]['path']
@@ -26,25 +26,27 @@ def _get_name(id, id_name_map, types)
   type_def = types[id]['def']
   name =
     if type_def.key?('primitive')
-      _get_primitive_name(id, type_def, id_name_map)
+      _get_primitive_name(type_def)
     elsif type_def.key?('array')
-      _get_array_name(id, type_def, id_name_map, types)
+      _get_array_name(id, type_def, id_name_map, name_id_map, types)
     elsif type_def.key?('sequence')
-      _get_sequence_name(id, type_def, id_name_map, types)
+      _get_sequence_name(id, type_def, id_name_map, name_id_map, types)
     elsif type_def.key?('tuple')
-      _get_tuple_name(id, type_def, id_name_map, types)
+      _get_tuple_name(id, type_def, id_name_map, name_id_map, types)
     elsif type_def.key?('compact')
-      _get_compact_name(id, type_def, id_name_map, types)
+      _get_compact_name(id, type_def, id_name_map, name_id_map, types)
     elsif type_def.key?('bitSequence')
-      _get_bit_sequence_name(id, type_def, id_name_map, types)
+      _get_bit_sequence_name(id, type_def, id_name_map, name_id_map, types)
     else
-      _get_other_name(id, path, params, id_name_map, types)
+      _get_other_name(id, path, params, id_name_map, name_id_map, types)
     end
 
+  name = fix_type_name(id, name, name_id_map)
   id_name_map[id] = name
+  name_id_map[name] = id
 end
 
-def _get_other_name(_id, path, params, id_name_map, types)
+def _get_other_name(_id, path, params, id_name_map, name_id_map, types)
   return if path.empty?
 
   name = path.join('::')
@@ -56,80 +58,80 @@ def _get_other_name(_id, path, params, id_name_map, types)
     if param_type_id.nil?
       param['name']
     else
-      _get_name(param_type_id, id_name_map, types)
+      _get_name(param_type_id, id_name_map, name_id_map, types)
     end
   end.join(', ')
 
   "#{name}<#{param_names_str}>"
 end
 
-# def get_type_name(id, name, name_id_map)
-#   if name_id_map.key?(name)
-#     exist_id = name_id_map[name]
-#     if exist_id != id
-#       # different type with same name
-#       splits = name.split('/')
-#       no = splits.length > 1 ? splits.last.to_i + 1 : 1
-#       return get_type_name(id, "#{splits[0]}/#{no}", name_id_map)
-#     end
-#   end
-#
-#   name
-# end
+def fix_type_name(id, name, name_id_map)
+  if name_id_map.key?(name)
+    exist_id = name_id_map[name]
+    if exist_id != id
+      # different type with same name
+      splits = name.split('/')
+      no = splits.length > 1 ? splits.last.to_i + 1 : 1
+      return fix_type_name(id, "#{splits[0]}/#{no}", name_id_map)
+    end
+  end
 
-def _get_primitive_name(_id, type_def, _id_name_map)
+  name
+end
+
+def _get_primitive_name(type_def)
   type_def['primitive']
 end
 
-def _get_bit_sequence_name(_id, type_def, id_name_map, types)
+def _get_bit_sequence_name(_id, type_def, id_name_map, name_id_map, types)
   inner_store_type_id = type_def['bitSequence']['bitStoreType']
   inner_store_type_name = id_name_map[inner_store_type_id]
-  inner_store_type_name = _get_name(inner_store_type_id, id_name_map, types) if inner_store_type_name.nil?
+  inner_store_type_name = _get_name(inner_store_type_id, id_name_map, name_id_map, types) if inner_store_type_name.nil?
   # return if inner_type_name.nil?
 
   inner_order_type_id = type_def['bitSequence']['bitOrderType']
   inner_order_type_name = id_name_map[inner_order_type_id]
-  inner_order_type_name = _get_name(inner_order_type_id, id_name_map, types) if inner_order_type_name.nil?
+  inner_order_type_name = _get_name(inner_order_type_id, id_name_map, name_id_map, types) if inner_order_type_name.nil?
   # return if inner_type_name.nil?
 
   "BitVec<#{inner_store_type_name}, #{inner_order_type_name}>"
 end
 
-def _get_compact_name(_id, type_def, id_name_map, types)
+def _get_compact_name(_id, type_def, id_name_map, name_id_map, types)
   inner_type_id = type_def['compact']['type']
   inner_type_name = id_name_map[inner_type_id]
-  inner_type_name = _get_name(inner_type_id, id_name_map, types) if inner_type_name.nil?
+  inner_type_name = _get_name(inner_type_id, id_name_map, name_id_map, types) if inner_type_name.nil?
   return if inner_type_name.nil?
 
   "Compact<#{inner_type_name}>"
 end
 
-def _get_tuple_name(_id, type_def, id_name_map, types)
+def _get_tuple_name(_id, type_def, id_name_map, name_id_map, types)
   inner_type_ids = type_def['tuple']
   inner_type_names =
     inner_type_ids.map do |inner_type_id|
       inner_type_name = id_name_map[inner_type_id]
-      inner_type_name = _get_name(inner_type_id, id_name_map, types) if inner_type_name.nil?
+      inner_type_name = _get_name(inner_type_id, id_name_map, name_id_map, types) if inner_type_name.nil?
       inner_type_name
     end
 
   "(#{inner_type_names.join(', ')})"
 end
 
-def _get_sequence_name(_id, type_def, id_name_map, types)
+def _get_sequence_name(_id, type_def, id_name_map, name_id_map, types)
   inner_type_id = type_def['sequence']['type']
   inner_type_name = id_name_map[inner_type_id]
-  inner_type_name = _get_name(inner_type_id, id_name_map, types) if inner_type_name.nil?
+  inner_type_name = _get_name(inner_type_id, id_name_map, name_id_map, types) if inner_type_name.nil?
   return if inner_type_name.nil?
 
   "Vec<#{inner_type_name}>"
 end
 
-def _get_array_name(_id, type_def, id_name_map, types)
+def _get_array_name(_id, type_def, id_name_map, name_id_map, types)
   len = type_def['array']['len']
   inner_type_id = type_def['array']['type']
   inner_type_name = id_name_map[inner_type_id]
-  inner_type_name = _get_name(inner_type_id, id_name_map, types) if inner_type_name.nil?
+  inner_type_name = _get_name(inner_type_id, id_name_map, name_id_map, types) if inner_type_name.nil?
   return if inner_type_name.nil?
 
   "[#{inner_type_name}; #{len}]"
