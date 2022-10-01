f179d8b8bcd9b2413305ce458945fd516e2e512a
Aki Wu <aki.wu@itering.com>
2022-10-01 14:33:48 +0800

    add hasher

--- lib/hasher.rb
+++ lib/hasher.rb
@@ -4,39 +4,50 @@ require 'xxhash'
 require 'blake2b'
 
 module Hasher
-  def self.identity(bytes)
-    bytes.to_hex[2..]
-  end
-
-  def self.twox64(data)
-    result = XXhash.xxh64 data, 0
-    bytes = result.to_bytes.reverse
-    bytes.to_hex[2..]
-  end
-
-  def self.twox128(data)
-    bytes = []
-    2.times do |i|
-      result = XXhash.xxh64 data, i
-      bytes += result.to_bytes.reverse
+  class << self
+    # hasher: 'Identity', 'Twox64Concat', 'Blake2128Concat'
+    # bytes: u8 array
+    def apply_hasher(hasher, bytes)
+      function_name = hasher.gsub('_', '').underscore
+      Hasher.send(function_name, bytes)
     end
-    bytes.to_hex[2..]
   end
 
-  def self.twox64_concat(bytes)
-    data = bytes.to_utf8
-    twox64(data) + bytes.to_hex[2..]
-  end
+  class << self
+    def identity(bytes)
+      bytes.to_hex[2..]
+    end
 
-  def self.blake2_128(bytes)
-    Blake2b.hex bytes, 16
-  end
+    def twox64_concat(bytes)
+      data = bytes.to_utf8
+      twox64(data) + bytes.to_hex[2..]
+    end
 
-  def self.blake2_256(bytes)
-    Blake2b.hex bytes, 32
-  end
+    def blake2128_concat(bytes)
+      blake2_128(bytes) + bytes.to_hex[2..]
+    end
 
-  def self.blake2128_concat(bytes)
-    blake2_128(bytes) + bytes.to_hex[2..]
+    def twox64(str)
+      result = XXhash.xxh64 str, 0
+      bytes = result.to_bytes.reverse
+      bytes.to_hex[2..]
+    end
+
+    def twox128(str)
+      bytes = []
+      2.times do |i|
+        result = XXhash.xxh64 str, i
+        bytes += result.to_bytes.reverse
+      end
+      bytes.to_hex[2..]
+    end
+
+    def blake2_128(bytes)
+      Blake2b.hex bytes, 16
+    end
+
+    def blake2_256(bytes)
+      Blake2b.hex bytes, 32
+    end
   end
 end
