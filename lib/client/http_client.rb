df098e0e558f3b2f81d820c814305f3f62bf12f9
Aki Wu <wuminzhe@gmail.com>
2023-04-17 20:25:58 +0800

    add rpc_methods to httpclient

--- lib/client/http_client.rb
+++ lib/client/http_client.rb
@@ -4,6 +4,7 @@ require 'uri'
 require 'net/http'
 require 'json'
 
+# TODO: method_name = cmd.gsub(/([a-z\d])([A-Z])/, '\1_\2').downcase
 module ScaleRb
   module HttpClient
     extend RpcRequestBuilder
@@ -19,7 +20,8 @@ module ScaleRb
         # puts res unless res.is_a?(Net::HTTPSuccess)
 
         result = JSON.parse(res.body)
-        raise result['error'] if result['error']
+        ScaleRb.logger.debug result
+        raise result['error']['message'] if result['error']
 
         result['result']
       end
@@ -34,7 +36,22 @@ module ScaleRb
       end
 
       def method_missing(method, *args)
-        json_rpc_call(args[0], method, *args[1..])
+        ScaleRb.logger.debug "#{method}(#{args.join(', ')})"
+        # check if the first argument is a url
+        url_regex = /^https?:\/\//
+        raise "url format is not correct" unless args[0].match?(url_regex)
+
+        url = args[0]
+        if rpc_methods(url).include?(method.to_s)
+          json_rpc_call(url, method, *args[1..])
+        else
+          raise NoMethodError, "undefined rpc method `#{method}'"
+        end
+      end
+
+      def rpc_methods(url)
+        result = json_rpc_call(url, 'rpc_methods', [])
+        result["methods"]
       end
 
       def get_metadata(url, at = nil)
