8e8bf84c3559c80c48c5e768a0269b90530dcdbe
Aki Wu <wuminzhe@gmail.com>
2024-07-16 17:28:28 +0800

    1. not allow call request directly 2. sleep a while when a unknown notification comes 3. better unsubscribe

--- examples/ws_client_1.rb
+++ examples/ws_client_1.rb
@@ -1,14 +1,10 @@
 require 'scale_rb'
 
-ScaleRb.logger.level = Logger::DEBUG
+# ScaleRb.logger.level = Logger::DEBUG
 
-# the commented code below is the same as the code above
 ScaleRb::WsClient.start('wss://polkadot-rpc.dwellir.com') do |client|
   block_hash = client.chain_getBlockHash(21585684)
-  # block_hash = client.request('chain_getBlockHash', [21585684])
-
   runtime_version = client.state_getRuntimeVersion(block_hash)
-  # runtime_version = client.request('state_getRuntimeVersion', [block_hash])
-
-  puts runtime_version
+  puts runtime_version['specName']
+  puts runtime_version['specVersion']
 end
