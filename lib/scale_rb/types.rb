623e0dfedaa827fa87446d16844cdeaa6724b012
Aki Wu <wuminzhe@gmail.com>
2024-09-26 22:41:37 +0800

    update types

--- lib/scale_rb/types.rb
+++ lib/scale_rb/types.rb
@@ -11,12 +11,12 @@ module ScaleRb
       'I8', 'U8', 'I16', 'U16', 'I32', 'U32', 'I64', 'U64', 'I128', 'U128', 'I256', 'U256',
       'Bool', 'Str', 'Char'
     )
-    Ti = Types::Strict::Integer.constrained(gt: 0)
+    Ti = Types::Strict::Integer.constrained(gteq: 0)
     U8 = Types::Strict::Integer.constrained(gteq: 0, lt: 256)
     U8Array = Types::Strict::Array.of(U8)
     Hex = Types::Strict::String.constrained(format: /\A0x[0-9a-fA-F]+\z/)
 
-    Registry = Types::Hash.map(Ti, Any)
+    Registry = Types::Array.of(Any)
 
     class Base < Dry::Struct
       attribute? :registry, Registry
@@ -147,48 +147,45 @@ module ScaleRb
   end
 end
 
-# p1 = ScaleRb::Types::PrimitiveType.new(primitive: 'U8')
+# p0 = ScaleRb::Types::PrimitiveType.new(primitive: 'U8')
+# puts "p0: #{p0}"
+
+# registry = [p0]
+
+# p1 = ScaleRb::Types::CompactType.new
 # puts "p1: #{p1}"
 
-# registry = { 1 => p1 }
-
-# p2 = ScaleRb::Types::CompactType.new
+# p2 = ScaleRb::Types::CompactType.new(type: 0, registry:)
 # puts "p2: #{p2}"
 
-# p3 = ScaleRb::Types::CompactType.new(type: 1, registry:)
+# p3 = ScaleRb::Types::SequenceType.new(type: 0, registry:)
 # puts "p3: #{p3}"
 
-# p4 = ScaleRb::Types::SequenceType.new(type: 1, registry:)
+# p4 = ScaleRb::Types::ArrayType.new(type: 0, len: 3, registry:)
 # puts "p4: #{p4}"
 
-# p5 = ScaleRb::Types::ArrayType.new(type: 1, len: 3, registry:)
+# registry = [p0, p1, p2, p3, p4]
+# p5 = ScaleRb::Types::TupleType.new(tuple: [0, 1, 2], registry:)
 # puts "p5: #{p5}"
 
-# # p6 = ScaleRb::Types::BitSequenceType.new(bit_store_type: 1, bit_order_type: 2, registry:)
-# # puts "p6: #{p6}"
+# p6 = ScaleRb::Types::StructType.new(
+#   fields: [
+#     ScaleRb::Types::Field.new(name: 'name', type: 0),
+#     ScaleRb::Types::Field.new(name: 'age', type: 1)
+#   ],
+#   registry:
+# )
+# puts "p6: #{p6}"
 
-# registry = { 1 => p1, 2 => p2, 3 => p3, 4 => p4, 5 => p5 }
-# p7 = ScaleRb::Types::TupleType.new(tuple: [1, 2, 3], registry:)
+# p7 = ScaleRb::Types::UnitType.new
 # puts "p7: #{p7}"
 
-# p8 = ScaleRb::Types::StructType.new(
-#   fields: [
-#     ScaleRb::Types::Field.new(name: 'name', type: 1),
-#     ScaleRb::Types::Field.new(name: 'age', type: 2)
+# p8 = ScaleRb::Types::VariantType.new(
+#   variants: [
+#     ScaleRb::Types::TupleVariant.new(name: :Bar, index: 1, tuple: p5),
+#     ScaleRb::Types::SimpleVariant.new(name: :Foo, index: 0),
+#     ScaleRb::Types::StructVariant.new(name: :Baz, index: 2, struct: p6)
 #   ],
 #   registry:
 # )
 # puts "p8: #{p8}"
-
-# p9 = ScaleRb::Types::UnitType.new
-# puts "p9: #{p9}"
-
-# p10 = ScaleRb::Types::VariantType.new(
-#   variants: [
-#     ScaleRb::Types::TupleVariant.new(name: :Bar, index: 1, tuple: p7),
-#     ScaleRb::Types::SimpleVariant.new(name: :Foo, index: 0),
-#     ScaleRb::Types::StructVariant.new(name: :Baz, index: 2, struct: p8)
-#   ],
-#   registry:
-# )
-# puts "p10: #{p10}"
